543afdb4c0b7185005b61c8847483f24af948194
BogdanDaxov <145527748+BogdanDaxov@users.noreply.github.com>
2024-11-18 01:05:30 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-with open("00"+"1"+".dat") as f :
+with open("Данные1/00"+"1"+".dat") as f :
     a=f.read().split("\n")
 n=int(a[0])
 plt.title("Number of points: "+str(n))
@@ -11,11 +11,12 @@ for g in range(1,n+1):
     x.append(float(a[g].split(" ")[0]))
     y.append(float(a[g].split(" ")[1]))
 plt.scatter(x,y)
+plt.axis('scaled')
 plt.savefig("1_1.png")
 plt.close()
 
 
-with open("00"+"2"+".dat") as f :
+with open("Данные1/00"+"2"+".dat") as f :
     a=f.read().split("\n")
 n=int(a[0])
 plt.title("Number of points: "+str(n))
@@ -25,10 +26,11 @@ for g in range(1,n+1):
     x.append(float(a[g].split(" ")[0]))
     y.append(float(a[g].split(" ")[1]))
 plt.scatter(x,y)
+plt.axis('scaled')
 plt.savefig("1_2.png")
 plt.close()
 
-with open("00"+"3"+".dat") as f :
+with open("Данные1/00"+"3"+".dat") as f :
     a=f.read().split("\n")
 n=int(a[0])
 plt.title("Number of points: "+str(n))
@@ -38,10 +40,11 @@ for g in range(1,n+1):
     x.append(float(a[g].split(" ")[0]))
     y.append(float(a[g].split(" ")[1]))
 plt.scatter(x,y)
+plt.axis('scaled')
 plt.savefig("1_3.png")
 plt.close()
 
-with open("00"+"4"+".dat") as f :
+with open("Данные1/00"+"4"+".dat") as f :
     a=f.read().split("\n")
 n=int(a[0])
 plt.title("Number of points: "+str(n))
@@ -51,10 +54,11 @@ for g in range(1,n+1):
     x.append(float(a[g].split(" ")[0]))
     y.append(float(a[g].split(" ")[1]))
 plt.scatter(x,y,s=1)
+plt.axis('scaled')
 plt.savefig("1_4.png")
 plt.close()
 
-with open("00"+"5"+".dat") as f :
+with open("Данные1/00"+"5"+".dat") as f :
     a=f.read().split("\n")
 n=int(a[0])
 plt.title("Number of points: "+str(n))
@@ -64,5 +68,6 @@ for g in range(1,n+1):
     x.append(float(a[g].split(" ")[0]))
     y.append(float(a[g].split(" ")[1]))
 plt.scatter(x,y,s=0.1)
+plt.axis('scaled')
 plt.savefig("1_5.png")
 plt.close()
