f8ab31410b985571f5869c88641b001e417374c2
BogdanDaxov <145527748+BogdanDaxov@users.noreply.github.com>
2024-11-17 23:44:42 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,10 +8,7 @@ for i in range(0,6):
     x=[float(s) for s in a[2*i].split(" ")]
     y=[float(s) for s in a[2*i+1].split(" ")]
     ax.plot(x, y)
-    #ax.set_xlim(xmin=0, xmax=15)
-    #ax.set_ylim(ymin=-9, ymax=10)
     ax.grid(visible=True)
-    plt.xticks(np.arange(0, 15, 2.0))
-    plt.yticks(np.arange(-9, 12, 2.0))
+    plt.axis((0,16,-9,12))
     plt.savefig("Граффики 2/"+str(i)+".png")
     plt.close()
